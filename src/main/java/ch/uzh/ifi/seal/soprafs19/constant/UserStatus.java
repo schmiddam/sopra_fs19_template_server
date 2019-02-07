fe22871de4f4f47d59f7c0aedff093dd10270438
Timothy Zemp <timothy@zemp.ch>
2019-02-07 02:54:39 +0100

    Setup SpringBoot 2.1 with Java11
    Signed-off-by: Timothy Zemp <timothy@zemp.ch>

---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/constant/UserStatus.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.seal.soprafs19.constant;
+
+public enum UserStatus {
+	ONLINE, OFFLINE;
+}
---
+++ src/main/java/ch/uzh/ifi/seal/soprafs19/controller/UserController.java
@@ -0,0 +1,29 @@
+package ch.uzh.ifi.seal.soprafs19.controller;
+
+import ch.uzh.ifi.seal.soprafs19.entity.User;
+import ch.uzh.ifi.seal.soprafs19.repository.UserRepository;
+import ch.uzh.ifi.seal.soprafs19.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private final UserService service;
+
+    UserController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    Iterable<User> all() {
+        return service.getUsers();
+    }
+
+    @PostMapping("/users")
+    User createUser(@RequestBody User newUser) {
+        return this.service.createUser(newUser);
+    }
+}
